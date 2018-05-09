3bbbf9ae2b54e4005a671258f998107baf09c39c
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-05-09 17:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const app = "I don't do much."
 function kitttens(){
   var kittens = ["Milo", "Otis", "Garfield"]
 }
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKittens(name){
+  kittens.unshift(name);
+}
